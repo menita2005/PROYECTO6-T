b192c1b4afdbd5cebe314b53ea443a9f38e7f232
dcerinza11 <danielcedam2022@gmail.com>
2024-05-16 13:58:59 -0500

    corregis css y html

--- SGI/resources/views/home.blade.php
+++ SGI/resources/views/home.blade.php
@@ -1,7 +1,153 @@
-@extends('layouts.plantilla')
+<!DOCTYPE html>
+<html style="font-size: 16px;" lang="es"><head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="​We&amp;apos;re The Best
+In Solar Energy, ​​Power Your Home With Solar, ​We specialize in a wide variety of solar installation, ​Installing Benefits, ​Solar Benefits, Our Projects, Contact Us">
+    <meta name="description" content="">
+    <title>postLogin</title>
+    <link rel="stylesheet" href="css/nicepage.css" media="screen">
+<link rel="stylesheet" href="css/postLogin.css" media="screen">
+    <script class="u-script" type="text/javascript" src="js/jquery.js"></script>
+    <script class="u-script" type="text/javascript" src="js/nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 6.9.2, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
 
-@section('title','Home')
 
-@section('content')
-<h1>hello mi love</h1>
-@endsection
+
+
+    <script type="application/ld+json">{
+		"@context": "http://schema.org",
+		"@type": "Organization",
+		"name": "",
+		"logo": "images/meencanbta.png",
+		"sameAs": []
+}</script>
+    <meta name="theme-color" content="#478ac9">
+    <meta property="og:title" content="postLogin">
+    <meta property="og:type" content="website">
+  <meta data-intl-tel-input-cdn-path="intlTelInput/"></head>
+  <body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="es"><header class="u-clearfix u-header u-header" id="sec-547f"><div class="u-clearfix u-sheet u-sheet-1">
+        <nav class="u-menu u-menu-one-level u-offcanvas u-menu-1" data-responsive-from="SM">
+          <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px;">
+            <a class="u-button-style u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link u-text-active-palette-1-base u-text-hover-palette-2-base" href="#">
+              <svg class="u-svg-link" viewBox="0 0 24 24"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#menu-hamburger"></use></svg>
+              <svg class="u-svg-content" version="1.1" id="menu-hamburger" viewBox="0 0 16 16" x="0px" y="0px" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg"><g><rect y="1" width="16" height="2"></rect><rect y="7" width="16" height="2"></rect><rect y="13" width="16" height="2"></rect>
+</g></svg>
+            </a>
+          </div>
+          <div class="u-custom-menu u-nav-container">
+            <ul class="u-nav u-unstyled u-nav-1"><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-1-base" href="#" style="padding: 18px 20px;">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-1-base" style="padding: 18px 20px;">Compra</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link u-text-active-palette-1-base u-text-hover-palette-1-base" style="padding: 18px 20px;">Venta</a>
+</li></ul>
+          </div>
+          <div class="u-custom-menu u-nav-container-collapse">
+            <div class="u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+              <div class="u-inner-container-layout u-sidenav-overflow">
+                <div class="u-menu-close"></div>
+                <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2"><li class="u-nav-item"><a class="u-button-style u-nav-link" href="#">Home</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link">Compra</a>
+</li><li class="u-nav-item"><a class="u-button-style u-nav-link">Venta</a>
+</li></ul>
+              </div>
+            </div>
+            <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+          </div>
+        </nav>
+        <a href="#" class="u-image u-logo u-image-1" data-image-width="228" data-image-height="234">
+          <img class="u-logo-image u-logo-image-1" src="images/meencanbta.png">
+        </a>
+      </div></header>
+    <section class="skrollable skrollable-between u-align-center u-clearfix u-image u-shading u-section-1" src="" id="carousel_ed7a" data-image-width="1792" data-image-height="1024">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h1 class="u-align-center u-text u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="250"> We're The Best In Inventory Control</h1>
+        <div class="u-expanded-width u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-center u-container-style u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-1" data-animation-name="customAnimationIn" data-animation-duration="1250" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-container-layout-1"><span class="u-align-center u-file-icon u-icon u-text-white u-icon-1"><img src="images/2126961-74e55360.png" alt=""></span>
+                <h4 class="u-align-center u-custom-font u-text u-text-font u-text-2"> Productos</h4>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-2" data-animation-name="customAnimationIn" data-animation-duration="1250" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-container-layout-2"><span class="u-align-center u-file-icon u-icon u-text-white u-icon-2"><img src="images/2982602-76e10467.png" alt=""></span>
+                <h4 class="u-align-center u-custom-font u-text u-text-font u-text-3">Proveedores</h4>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-3" data-animation-name="customAnimationIn" data-animation-duration="1250" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-container-layout-3"><span class="u-align-center u-file-icon u-icon u-text-white u-icon-3"><img src="images/3843454-34c22817.png" alt=""></span>
+                <h4 class="u-align-center u-custom-font u-text u-text-font u-text-4">Caegorias</h4>
+              </div>
+            </div>
+            <div class="u-align-center u-container-style u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-4" data-animation-name="customAnimationIn" data-animation-duration="1250" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-container-layout-4"><span class="u-align-center u-file-icon u-icon u-text-white u-icon-4"><img src="images/3596030-32f189ad.png" alt=""></span>
+                <h4 class="u-align-center u-custom-font u-text u-text-font u-text-5">Infomes</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="u-align-left u-clearfix u-white u-section-2" id="carousel_02ec">
+      <div class="u-clearfix u-sheet u-sheet-1">
+        <h2 class="u-align-center u-text u-text-default u-text-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500"> Optimize Your Inventory</h2>
+        <div class="custom-expanded u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-left u-container-style u-custom-item u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-1" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-1">
+                <h3 class="u-text u-text-default u-text-2"> Productos</h3>
+                <p class="u-custom-item u-text u-text-default u-text-3"> Gestiona tu inventario con precisión, optimiza tus operaciones y alcanza el éxito empresarial sin complicaciones.</p>
+                <a href="#" class="u-active-white u-align-center u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-item u-hover-white u-none u-text-active-palette-1-base u-text-body-alt-color u-text-hover-palette-1-base u-btn-1" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="750">Learn More</a>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-custom-item u-image u-list-item u-repeater-item u-shading u-shape-rectangle u-image-1" data-image-width="1024" data-image-height="648" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-2">
+                <h3 class="u-text u-text-default u-text-4"> Proveedores</h3>
+                <p class="u-custom-item u-text u-text-body-alt-color u-text-default u-text-5"> Potencia tu cadena de suministro: gestiona tu inventario con eficacia para satisfacer las necesidades de tus clientes y alcanzar tus metas de crecimiento junto a nosotros, tus socios confiables.</p>
+                <a href="#" class="u-active-white u-align-center u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-item u-hover-white u-none u-text-active-palette-1-base u-text-body-alt-color u-text-hover-palette-1-base u-btn-2" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="750">Learn More</a>
+              </div>
+            </div>
+            <div class="u-align-left u-black u-container-style u-custom-item u-list-item u-repeater-item u-shape-rectangle u-list-item-3" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-3">
+                <h3 class="u-text u-text-default u-text-6">Categorias<br>
+                </h3>
+                <p class="u-custom-item u-text u-text-default u-text-7"> Maximiza la eficiencia de tu inventario por categorías: optimiza la gestión de tus productos, reduce costos y eleva la satisfacción del cliente con nuestra solución integral para proveedores</p>
+                <a href="#" class="u-active-white u-align-center u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-item u-hover-white u-none u-text-active-palette-1-base u-text-body-alt-color u-text-hover-palette-1-base u-btn-3" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="750">Learn More</a>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-custom-item u-list-item u-palette-1-base u-repeater-item u-shape-rectangle u-list-item-4" data-animation-name="customAnimationIn" data-animation-duration="1500" data-animation-delay="500">
+              <div class="u-container-layout u-similar-container u-valign-top u-container-layout-4">
+                <h3 class="u-text u-text-default u-text-8">Informes</h3>
+                <p class="u-custom-item u-text u-text-default u-text-9"> Potencia tu negocio con nuestra solución de gestión de inventario: genera informes detallados, obtén insights estratégicos y toma decisiones informadas para impulsar el crecimiento y la rentabilidad de tu empresa</p>
+                <a href="#" class="u-active-white u-align-center u-border-2 u-border-active-white u-border-hover-white u-border-white u-btn u-button-style u-color-scheme-summer-time u-color-style-multicolor-1 u-custom-item u-hover-white u-none u-text-active-palette-1-base u-text-body-alt-color u-text-hover-palette-1-base u-btn-4" data-animation-name="customAnimationIn" data-animation-duration="1750" data-animation-delay="750">Learn More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <footer class="u-clearfix u-footer u-palette-5-dark-1" id="sec-b92e"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
+          <a class="u-social-url" title="facebook" target="_blank" href=""><span class="u-file-icon u-icon u-social-facebook u-social-icon u-icon-1"><img src="images/3536423.png" alt=""></span>
+          </a>
+          <a class="u-social-url" title="twitter" target="_blank" href=""><span class="u-file-icon u-icon u-social-icon u-social-twitter u-icon-2"><img src="images/5968958.png" alt=""></span>
+          </a>
+          <a class="u-social-url" title="instagram" target="_blank" href=""><span class="u-file-icon u-icon u-social-icon u-social-instagram u-icon-3"><img src="images/2111491.png" alt=""></span>
+          </a>
+          <a class="u-social-url" title="linkedin" target="_blank" href=""><span class="u-file-icon u-icon u-social-icon u-social-linkedin u-icon-4"><img src="images/3488334.png" alt=""></span>
+          </a>
+        </div>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-grey-80">
+      <p class="u-text">
+        <span>This site was created with the </span>
+        <a class="u-link" href="https://nicepage.com/" target="_blank" rel="nofollow">
+          <span>Nicepage</span>
+        </a>
+      </p>
+    </section>
+
+</body></html>
